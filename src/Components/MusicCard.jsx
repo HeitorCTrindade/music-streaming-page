fbadcb3e52d5d00a60e4d4d4b6930be776ec1e47
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-08-02 15:43:41 -0300

    Requisito 08 - finalizado - primeiro teste npm

--- src/Components/MusicCard.jsx
+++ src/Components/MusicCard.jsx
@@ -3,16 +3,26 @@ import PropTypes from 'prop-types';
 
 class MusicCard extends Component {
   render() {
-    const { trackName, previewUrl } = this.props;
+    const { trackName, previewUrl, trackId, onChange } = this.props;
     return (
       <div data-testid="page-MusicCard">
-        <p>{ trackName }</p>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
+        <p>{trackName}</p>
+        <audio src={ previewUrl } controls data-testid="audio-component">
           <track kind="captions" />
           O seu navegador não suporta o elemento
           {' '}
           <code>audio</code>
         </audio>
+        <label htmlFor="User">
+          Favorita
+          <input
+            type="checkbox"
+            name={ trackId }
+            id=""
+            data-testid={ `checkbox-music-${trackId}` }
+            onChange={ onChange }
+          />
+        </label>
       </div>
     );
   }
@@ -21,6 +31,7 @@ class MusicCard extends Component {
 MusicCard.propTypes = {
   trackName: PropTypes.string.isRequired,
   previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.string.isRequired,
 };
 
 export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../Components/Header';
 import getMusics from '../services/musicsAPI';
+import { addSong } from '../services/favoriteSongsAPI';
 import MusicCard from '../Components/MusicCard';
 import Loading from '../Components/Loading';
 
 class Album extends Component {
   state = {
     isPageLoading: true,
+    isSavingMusic: false,
+    arrayMusicsCard: [],
     arrayMusicCardElements: <> </>,
   };
 
@@ -15,6 +18,7 @@ class Album extends Component {
     const { match: { params: { id } } } = this.props;
     this.setState(async () => {
       const arrayMuscisFromAlbum = await getMusics(id);
+      this.setState({ arrayMusicsCard: arrayMuscisFromAlbum });
       this.setState({ arrayMusicCardElements:
         this.musicCardElements(arrayMuscisFromAlbum) });
       this.setState({ isPageLoading: false });
@@ -29,6 +33,8 @@ class Album extends Component {
         key={ music.trackId }
         trackName={ music.trackName }
         previewUrl={ music.previewUrl }
+        trackId={ music.trackId }
+        onChange={ this.handleCheckBox }
       />
     ));
     return (
@@ -44,11 +50,23 @@ class Album extends Component {
     );
   }
 
+  handleCheckBox = async (event) => {
+    this.setState({ isSavingMusic: true });
+    const { name } = event.target;
+    const { arrayMusicsCard } = this.state;
+    const musicFavoriteObj = arrayMusicsCard
+      .filter((musics) => musics.trackId === parseInt(name, 10));
+    console.log(musicFavoriteObj);
+    await addSong(musicFavoriteObj);
+    this.setState({ isSavingMusic: false });
+  }
+
   render() {
-    const { isPageLoading, arrayMusicCardElements } = this.state;
+    const { isPageLoading, arrayMusicCardElements, isSavingMusic } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        { isSavingMusic ? <Loading /> : <> </> }
         { isPageLoading ? <Loading /> : arrayMusicCardElements }
       </div>
     );
